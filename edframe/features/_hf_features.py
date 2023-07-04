2b5b130d48f3b4e0803042a47d281c0c76711546
Ilia Kamyshev <arx7ti@gmail.com>
2023-07-04 12:36:28 +0300

    migrate support functions

--- edframe/features/_hf_features.py
+++ edframe/features/_hf_features.py
@@ -2,36 +2,6 @@ import numpy as np
 from ._generics import Feature
 
 
-def spectral_centroid(x):
-    x = np.abs(np.fft.rfft(x))[1:]
-    x = (x * np.arange(1, len(x) + 1)).sum() / x.sum()
-    return x
-
-
-def temporal_centroid(x):
-    x = np.abs(np.fft.rfft(x))[2:]
-    x = (x * 60 * np.arange(1, len(x) + 1)).sum() / x.sum()
-    return x
-
-
-def spf(x):
-    x = np.abs(np.fft.rfft(x))[2:]
-    x = np.power(np.prod(x), 1 / len(x)) / np.mean(x)
-    return x
-
-
-def rms(x):
-    return np.sqrt(np.mean(np.square(x)))
-
-def fft_amplitudes(x):
-    x = np.fft.rfft(x) * 2 / len(x)
-    x = np.abs(x)
-    return x
-
-def fft_amplitudes_check_fn(x):
-    if len(x.shape) > 1:
-        raise ValueError
-
 SpectralCentroid = Feature(spectral_centroid)
 TemporalCentroid = Feature(temporal_centroid)
 RMS = Feature(rms)
