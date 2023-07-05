000c14e61e100617fdd9b3630ed49175c4f7626c
Pavel Stepanov <ikdesign2015@yandex.ru>
2023-07-05 16:13:57 +0300

    resolve imports

--- edframe/features/_hf_features.py
+++ edframe/features/_hf_features.py
@@ -1,9 +1,12 @@
 import numpy as np
 from ._generics import Feature
-
+from ..signals._scalar import spectral_centroid, temporal_centroid, spf, rms
+from ..signals._vector import fft_amplitudes, fft_amplitudes_check_fn
 
 SpectralCentroid = Feature(spectral_centroid)
 TemporalCentroid = Feature(temporal_centroid)
 RMS = Feature(rms)
 SpectralFlatness = Feature(spf)
-FourierAmplitudes = Feature(fft_amplitudes, multioutput=True,check_fn=fft_amplitudes_check_fn)
+FourierAmplitudes = Feature(fft_amplitudes,
+                            multioutput=True,
+                            check_fn=fft_amplitudes_check_fn)
