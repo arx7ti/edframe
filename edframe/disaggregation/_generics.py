459ea8b77adef45a8575e2e5e99165fa647e6268
Ilia Kamyshev <arx7ti@gmail.com>
2023-07-18 00:35:02 +0300

    disaggregator init

---
+++ edframe/disaggregation/_generics.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Callable, Any
+
+import numpy as np
+
+
+class Disaggregator:
+    def __init__(self, model) -> None:
+        pass
+
+    def fit(self):
+        pass
+
+    def disaggregate(self):
+        pass
