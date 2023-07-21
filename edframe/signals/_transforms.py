6d8236b15ed6e23caa082eb8ce0542e0974cdeb2
arx7ti <arx7ti@gmail.com>
2023-07-21 20:07:25 +0300

    revise transforms

--- edframe/signals/_transforms.py
+++ edframe/signals/_transforms.py
@@ -9,19 +9,23 @@ from statsmodels.tsa.ar_model import AutoReg
 import numpy as np
 
 
-def identity(x:np.ndarray):
+def identity(x: np.ndarray):
     return x
+
 
 def downsample(x: np.ndarray, n_samples: int) -> np.ndarray:
     if n_samples > x.shape[-1]:
         raise ValueError
+
     x = resample(x, n_samples, axis=-1)
+
     return x
 
 
 def enhance(x: np.ndarray, n_samples: int, kind: str = 'linear') -> np.ndarray:
     if n_samples < x.shape[-1]:
         raise ValueError
+
     t0 = np.linspace(0, 1, x.shape[-1], dtype=x.dtype)
     t1 = np.linspace(0, 1, n_samples, dtype=x.dtype)
     mean = x.mean(axis=-1, keepdims=True)
@@ -29,31 +33,48 @@ def enhance(x: np.ndarray, n_samples: int, kind: str = 'linear') -> np.ndarray:
     enhancer = interp1d(t0, x, kind=kind, axis=-1)
     x = enhancer(t1)
     x += mean
+
     return x
 
 
-def pad(x: np.ndarray, padwidth: Union[int, tuple[int, int]],
-        **kwargs: Any) -> np.ndarray:
-    if isinstance(padwidth, tuple):
-        padwidth = ((0, 0), padwidth)
-    if len(kwargs) == 0:
-        kwargs.update(mode='constant', constant_values=0)
-    x = np.pad(x, padwidth, **kwargs)
+def pad(
+    x: np.ndarray,
+    n: int | tuple[int, int],
+    axis: int = -1,
+) -> np.ndarray:
+    if isinstance(n, int):
+        a, b = 0, n
+    else:
+        a, b = n
+
+    if axis < 0:
+        axis = len(x.shape) + axis
+
+    x = np.swapaxes(x, axis, -1)
+    xa = np.zeros((*x.shape[:-1], a))
+    xb = np.zeros((*x.shape[:-1], b))
+    x = np.concatenate((xa, x, xb), axis=-1)
+    x = np.swapaxes(x, -1, axis)
+
     return x
 
 
-def roll(x: np.ndarray,
-         n_periods: int,
-         mode: str = 'constant',
-         **kwargs: Any) -> np.ndarray:
+def roll(
+    x: np.ndarray,
+    n_periods: int,
+    mode: str = 'constant',
+    **kwargs: Any,
+) -> np.ndarray:
     if n_periods > 0:
         slicer = np.s_[:n_periods]
     elif n_periods < 0:
         slicer = np.s_[n_periods:]
     else:
         slicer = None
+
     if slicer is not None:
         x = np.roll(x, n_periods, axis=-2)
+
         if mode == 'constant':
             fill_values = kwargs.get('constant_value', 0)
         elif mode == 'noise':
@@ -64,44 +85,76 @@ def roll(x: np.ndarray,
             fill_values = mean + std * fill_values.astype(x.dtype)
         else:
             raise ValueError
+
         x[slicer] = fill_values
+
     return x
 
 
-def crop(x: np.ndarray, on: int, off: int) -> np.ndarray:
-    x = x[on:off + 1]
+def crop(x: np.ndarray, a: int, b: int, axis=-1) -> np.ndarray:
+    if b <= a:
+        raise ValueError
+
+    if b > x.shape[axis] or a > x.shape[axis]:
+        raise ValueError
+
+    if axis < 0:
+        axis = len(x.shape) + axis
+
+    x = np.swapaxes(x, axis, -1)
+    x = x[..., a:b]
+    x = np.swapaxes(x, -1, axis)
+
     return x
 
 
-def replicate(x: np.ndarray, repwidth: Union[int, tuple[int,
-                                                        int]]) -> np.ndarray:
-    if isinstance(repwidth, int):
-        repwidth = (0, repwidth)
-    if repwidth[0] > 0:
-        x0 = x[..., :1, :]
+def replicate(
+    x: np.ndarray,
+    n: int | tuple[int, int],
+    axis: int = -1,
+) -> np.ndarray:
+    if isinstance(n, int):
+        a, b = 0, n
     else:
-        x0 = np.empty((*x.shape[:-2], 0, x.shape[-1]))
-    if repwidth[1] > 0:
-        x1 = x[..., -1:, :]
+        a, b = n
+
+    if axis < 0:
+        axis = len(x.shape) + axis
+
+    x = np.swapaxes(x, axis, -1)
+
+    if a > 0:
+        xa = np.repeat(x[..., :1], a, axis=-1)
     else:
-        x1 = np.empty((*x.shape[:-2], 0, x.shape[-1]))
-    x = np.concatenate((x0, x, x1), axis=-2)
+        xa = np.empty((*x.shape[:-1], 0))
+
+    if b > 0:
+        xb = np.repeat(x[..., -1:], b, axis=-1)
+    else:
+        xb = np.empty((*x.shape[:-1], 0))
+
+    x = np.concatenate((xa, x, xb), axis=-1)
+    x = np.swapaxes(x, -1, axis)
+
     return x
 
 
-def extrapolate(x: np.ndarray, extrawidth: int) -> np.ndarray:
-    if len(x.shape) > 2:
+def extrapolate(x: np.ndarray, n: int, lags: int) -> np.ndarray:
+    if len(x.shape) > 1:
         raise NotImplementedError
+
     if x.shape[0] == 1:
         raise NotImplementedError
+
     if x.shape[0] == 2:
         raise NotImplementedError
-    start = x.size
-    end = x.size + x.shape[1] * extrawidth - 1
-    autoreg = AutoReg(x.ravel(), x.shape[1]).fit()
-    extra = autoreg.predict(start, end).view()
-    extra = extra.reshape(extrawidth, x.shape[1])
-    x = np.concatenate((x, extra), axis=-2)
+
+    a = x.size
+    b = x.size + n - 1
+    autoreg = AutoReg(x, lags, trend='ct').fit()
+    xe = autoreg.predict(a, b).view()
+    x = np.concatenate((x, xe))
+
     return x
 
 
