36a6f1113c4901ebd73c459687f109975f1a8b6a
arx7ti <arx7ti@gmail.com>
2023-08-15 20:30:01 +0300

    events threshold major fix

--- edframe/events/_generics.py
+++ edframe/events/_generics.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from typing import Optional, Callable, Iterable
 from sklearn.cluster import AgglomerativeClustering
 from ..data.entities import PowerSample, DataSet
+from scipy.signal import savgol_filter
 
 import numpy as np
 # import itertools as it
@@ -68,7 +69,7 @@ class EventDetector:
 
     def _striding_window_view(self, x: np.ndarray) -> np.ndarray:
         axes = x.shape[:-1]
-        n_pad = self._window_size - (x.shape[-1] % self._window_size)
+        n_pad = x.shape[-1] % self._window_size
 
         if n_pad > 0:
             # Padding with zeros
@@ -76,6 +77,7 @@ class EventDetector:
             x = np.concatenate((x, x_pad), axis=-1)
 
         n_windows = x.shape[-1] // self._window_size
+        # FIXME not always reshaping e.g. ws=67//2
         x = x.reshape(*axes, n_windows, self._window_size)
 
         return x
@@ -100,6 +102,43 @@ class EventDetector:
 
     def check_fn(self, x, **kwargs):
         return None
+
+    def _adjust_locs(self, locs: np.ndarray, xlocs: np.ndarray) -> np.ndarray:
+        i = np.arange(max(xlocs.max(), locs.max()))
+        D = xlocs[:, 1] - xlocs[:, 0]
+        i0 = xlocs[:, 0]
+        mask = (i[:, None] >= i0) & (i[:, None] < (i0 + D))
+        # Activations
+        acts = mask.sum(1)
+        # Indices where at least 1 load is running
+        i = np.argwhere(acts > 0).ravel()
+        di = np.diff(i, append=i[-1] + 1)
+        # Jumps in activations
+        jacts = np.argwhere(di != 1).ravel()
+        J = [[_j[0], _j[-1] + 1] for _j in np.split(i, jacts + 1)]
+        J = np.asarray(J)
+        # Jumps in original locs
+        jlocs = np.unique(xlocs.ravel())
+        # Corrected locs
+        clocs = np.empty((0, 2), dtype=int)
+        Jmax = J.max()
+
+        for x0, x1 in locs:
+            _jacts = jlocs[(jlocs > x0) & (jlocs < x1)]
+
+            if np.any(x0 >= J[:, 0]):
+                _jacts = np.insert(_jacts, 0, x0)
+
+            if np.all(x1 <= J[:, 1]) or x1 == Jmax:
+                _jacts = np.append(_jacts, x1)
+
+            _clocs = np.repeat(_jacts, 2)[1:-1].reshape(-1, 2)
+            clocs = np.concatenate((clocs, _clocs))
+
+        D2 = clocs[:, 1] - clocs[:, 0]
+        locs = clocs[D2 >= self._window_size]
+
+        return locs
 
 
 class ThresholdEvent(EventDetector):
@@ -126,6 +165,7 @@ class ThresholdEvent(EventDetector):
 
     def detect(self, x: PowerSample | DataSet | np.ndarray, **kwargs):
         self._check_compatibility(x)
+        l = len(x)
 
         if isinstance(x, DataSet):
             return self._detect_from_dataset(x, **kwargs)
@@ -135,8 +175,6 @@ class ThresholdEvent(EventDetector):
             x = x.source(self.source_name)
         else:
             xlocs = None
-        # elif not isinstance(x, np.ndarray):
-        #     raise ValueError
 
         if not isinstance(x, np.ndarray):
             raise ValueError
@@ -148,91 +186,29 @@ class ThresholdEvent(EventDetector):
 
         x = self._striding_window_view(x)
         x = np.apply_along_axis(self._window, axis=1, arr=x)
-        x = np.where(x > self._alpha, 1, 0)
-        dx = np.diff(x > self._alpha, prepend=False).astype(int)
-        signs = np.sign(dx)
-        locs0 = np.argwhere(signs > 0).ravel()
-        locs1 = np.argwhere(signs < 0).ravel()
+        x = np.where(x > self._alpha if self._above else x < self._alpha, 1, 0)
+        dx = np.diff(x, prepend=0)
+
+        # signs = np.sign(dx)
+        locs0 = np.argwhere(dx > 0).ravel()
+        locs1 = np.argwhere(dx < 0).ravel()
         locs = np.concatenate((locs0, locs1))
+
+        if len(locs) == 0:
+            return locs
 
         # Calibration
         if len(locs) % 2 > 0:
-            locs = np.append(locs, len(x) - 1)
+            locs = np.append(locs, len(x))
 
-        signs = signs[locs]
+        # signs = signs[locs]
         locs *= self._window_size
-        locs[len(locs0):] -= 1
+        locs = locs.reshape(-1, 2)
 
-        # if xlocs is not None:
-        #     # locs are not binded to labels
-        #     # what to do if we have labels?
-        #     # locs_upd = []
-        #     locs = np.clip(locs,
-        #                    a_min=xlocs[:, 0].min(),
-        #                    a_max=xlocs[:, 1].max())
         if xlocs is not None:
-            # N -> m
-            # N: [0 40] [50 70] [80 110] [120 150] labels domain
-            # m: [15 100], [30 90] events in signal
-            # -> [15 40] [50 70] [80 100]
-            # -> [30 40] [50 70] [80 90]
-            # --> [15 40] [50 70] [80 90]
-            ### ALG:
-            ### define labelled intervals for xlocs
-            ### extract locs from labelled intervals
-            ### for each set of labels define its own locs
+            locs = self._adjust_locs(locs, xlocs)
 
-            i = np.arange(max(xlocs.max(), locs.max()))
-            D = xlocs[:, 1] - xlocs[:, 0]
-            i0 = xlocs[:, 0]
-
-            mask = (i[:, None] >= i0) & (i[:, None] < (i0 + D))
-            acts = mask.sum(1)
-            i = np.argwhere(acts > 0).ravel()
-
-            di = np.diff(i, append=i[-1] + 1)
-
-            # Jumps in activations
-            jacts = np.argwhere(di != 1).ravel()
-
-            J = [[_j[0], _j[-1] + 1] for _j in np.split(i, jacts + 1)]
-            J = np.asarray(J)
-
-            # Jumps in original locs
-            jlocs = np.unique(xlocs.ravel()).reshape(-1, 2)
-
-            # Corrected locs
-            clocs = np.empty((0, 2), dtype=int)
-            Jmax = J.max()
-
-            locs2d = locs.reshape(-1, 2)
-            locs2d[:, 1] += 1
-
-            for x0, x1 in locs2d:
-                _jacts = jlocs[(jlocs > x0) & (jlocs < x1)]
-
-                if np.any(x0 >= J[:, 0]):
-                    _jacts = np.insert(_jacts, 0, x0)
-
-                if np.all(x1 <= J[:, 1]) or x1 == Jmax:
-                    _jacts = np.append(_jacts, x1)
-
-                _clocs = np.repeat(_jacts, 2)[1:-1].reshape(-1, 2)
-                clocs = np.concatenate((clocs, _clocs))
-
-            # FIXME end-index is exclusive
-            # FIXME 0-length intervals
-            # clocs[:, 1] -= 1
-            # locs = clocs.ravel()
-            locs = np.unique(clocs.ravel())
-
-            # for ax,bx in xlocs:
-            #     ilocs = np.clip(locs, a_min=ax, a_max=bx)
-
-        # events = list(zip(locs, signs))
-        events = locs
-
-        return events
+        return locs
 
 
 class DerivativeEvent(EventDetector):
@@ -276,13 +252,28 @@ class DerivativeEvent(EventDetector):
 
         self.check_fn(x, **kwargs)
 
+        # import matplotlib.pyplot as plt
+
         x = self._striding_window_view(x)
         x = np.apply_along_axis(self._window, axis=1, arr=x)
+
+        # plt.plot(x)
+        x = savgol_filter(x, min(5, len(x)), 1)
+        x = savgol_filter(x[::-1], min(5, len(x)), 1)[::-1]
+        # plt.plot(x)
+        # plt.show()
         x = np.nan_to_num(x, nan=np.Inf)
         dx = np.diff(x, prepend=np.NINF)
 
         if self._relative:
             dx[1:] /= x[:-1]
+
+        # plt.plot(np.diff(x, prepend=np.NINF))
+        # plt.show()
+
+        # plt.plot(np.abs(dx))
+        # plt.ylim(0, self._alpha * 1.1)
+        # plt.show()
 
         dx = np.nan_to_num(dx, nan=np.Inf)
 
@@ -312,7 +303,6 @@ class DerivativeEvent(EventDetector):
         # signs = np.sign(dx[locs])
         # signs = np.where(signs < 0, 0, 1)
         locs *= self._window_size
-        # print('2>>>', locs)
         assert locs[-1] <= len(x) * self._window_size
 
         locs = np.repeat(locs, 2)[1:-1].reshape(-1, 2)
@@ -322,44 +312,7 @@ class DerivativeEvent(EventDetector):
         #     return locs
 
         if xlocs is not None:
-            i = np.arange(max(xlocs.max(), locs.max()))
-            D = xlocs[:, 1] - xlocs[:, 0]
-            i0 = xlocs[:, 0]
-            mask = (i[:, None] >= i0) & (i[:, None] < (i0 + D))
-            acts = mask.sum(1)
-
-            i = np.argwhere(acts > 0).ravel()
-            di = np.diff(i, append=i[-1] + 1)
-            # Jumps in activations
-            jacts = np.argwhere(di != 1).ravel()
-            J = [[_j[0], _j[-1] + 1] for _j in np.split(i, jacts + 1)]
-            J = np.asarray(J)
-
-            # Jumps in original locs
-            jlocs = np.unique(xlocs.ravel())
-
-            # Corrected locs
-            clocs = np.empty((0, 2), dtype=int)
-            Jmax = J.max()
-
-            for x0, x1 in locs:
-                _jacts = jlocs[(jlocs > x0) & (jlocs < x1)]
-
-                if np.any(x0 >= J[:, 0]):
-                    _jacts = np.insert(_jacts, 0, x0)
-
-                if np.all(x1 <= J[:, 1]) or x1 == Jmax:
-                    _jacts = np.append(_jacts, x1)
-
-                _clocs = np.repeat(_jacts, 2)[1:-1].reshape(-1, 2)
-                clocs = np.concatenate((clocs, _clocs))
-
-            # FIXME allow <window_size loss
-            D2 = clocs[:, 1] - clocs[:, 0]
-            # print(self._window_size // 4, self._window_size)
-            # locs = clocs[D2 >= self._window_size // 4]
-            locs = clocs[D2 >= self._window_size]
-            # print(locs)
+            locs = self._adjust_locs(locs, xlocs)
 
         return locs
 
@@ -387,25 +340,10 @@ class ROI:
         def _crop(x: PowerSample | np.ndarray, detectors):
             roi = []
             detector0 = detectors[0]
-            # FIXME if not isfullyfit
             locs2d0 = detector0(x)
-            # print('-'*30)
-            # print(x.locs)
-            # print(locs2d0)
-            # print('-'*30)
 
-            # print(locs2d0)
             for a0, b0 in locs2d0:
-                # print(a0, b0)
                 xab0 = x[a0:b0]
-
-                # print(a0, b0)
-                # print(x.locs)
-                # print(xab0.locs)
-                # print('>>>\n')
-
-                # if not xab0.isfullyfit():
-                #     print('\nooo', a0, b0, xab0.locs, x.locs, '\n\n')
 
                 if len(detectors) > 1:
                     _roi = _crop(xab0, detectors[1:])
