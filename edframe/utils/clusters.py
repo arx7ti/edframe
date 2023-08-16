0a41e1741db0afe5a06c78e527bbbe4ba4f90b8f
arx7ti <arx7ti@gmail.com>
2023-08-16 20:55:30 +0300

    upd

--- edframe/utils/clusters.py
+++ edframe/utils/clusters.py
@@ -11,33 +11,49 @@ def centroids(
     X: np.ndarray,
     y: np.ndarray,
     median: bool = True,
+    return_labels:bool=False,
 ) -> tuple[np.ndarray, np.ndarray]:
     if len(X) != len(y):
         raise ValueError
-    centroids = []
-    y_new = []
+
+    X_centroids = []
+    y_centroids= []
     knn = NearestNeighbors()
-    for k in np.unique(y):
-        X_class = X[y == k]
-        knn.fit(X_class)
-        d, _ = knn.kneighbors(X_class)
-        d = np.sort(d, axis=0)[:, 1]
-        kloc = KneeLocator(np.arange(len(d)), d, curve='convex')
-        dbscan = DBSCAN(eps=d[kloc.knee])
-        labels = dbscan.fit_predict(X_class)
-        clusters = np.unique(labels)
-        if len(clusters) > 1:
-            X_class = X_class[labels != -1]
-            labels = labels[labels != -1]
-            clusters = clusters[clusters != -1]
-        for cluster in clusters:
-            X_cluster = X_class[labels == cluster]
-            if median:
-                centroid = np.median(X_cluster, axis=0)
-            else:
-                centroid = np.mean(X_cluster, axis=0)
-            centroids.append(centroid)
-        y_new.append([k] * len(clusters))
-    centroids = np.stack(centroids, axis=0)
-    y_new = np.asarray(y_new)
-    return centroids, y_new
+
+    # for k in np.unique(y):
+        # X_class = X[y == k]
+
+    knn.fit(X)
+    d, _ = knn.kneighbors(X)
+    d = np.sort(d, axis=0)[:, 1]
+    kloc = KneeLocator(np.arange(len(d)), d, curve='convex')
+    dbscan = DBSCAN(eps=d[kloc.knee])
+    y_new = dbscan.fit_predict(X)
+    clusters = np.unique(y_new)
+
+    # if len(clusters) > 1:
+        # X = X[y_new != -1]
+        # y_new = y_new[y_new != -1]
+        # clusters = clusters[clusters != -1]
+
+    # _y = []
+    # _X = []
+
+    for cluster in clusters:
+        X_cluster = X[y_new == cluster]
+
+        if median:
+            centroid = np.median(X_cluster, axis=0)
+        else:
+            centroid = np.mean(X_cluster, axis=0)
+
+        X_centroids.append(centroid)
+        y_centroids.append(cluster)
+
+    X_centroids = np.asarray(X_centroids)
+    y_centroids = np.asarray(y_centroids)
+
+    if return_labels:
+        return X_centroids, y_centroids, y_new
+
+    return X_centroids, y_centroids
