20d56cecd296096b60aa08803a95cd2e49b3dda8
arx7ti <arx7ti@gmail.com>
2023-08-17 23:16:12 +0300

    power decomposition

--- edframe/signals/__init__.py
+++ edframe/signals/__init__.py
@@ -1,3 +1,4 @@
 from ._fitps import FITPS
 from ._noise import gaussian_noise
 from ._transforms import F, crop, pad, roll, extrapolate, enhance, downsample, replicate
+from ._power import fryze, budeanu
---
+++ edframe/signals/_power.py
@@ -0,0 +1,16 @@
+import numpy as np
+from edframe.features._scalar import rms
+
+
+def fryze(v: np.ndarray, i: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    s = v * i
+    ia = s.mean() * v / rms(v)**2
+    ir = i - ia
+    return ia, ir
+
+
+def budeanu(
+    v: np.ndarray,
+    i: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    pass
