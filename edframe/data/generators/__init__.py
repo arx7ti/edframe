ade1016e91edfad31441857b9516bdae94e28f7e
arx7ti <arx7ti@gmail.com>
2023-11-18 20:39:29 +0300

    clean waveforms

--- edframe/data/generators/__init__.py
+++ edframe/data/generators/__init__.py
@@ -1,2 +1,2 @@
-from ._waveforms2 import make_periods, make_oscillations, make_rms_cycle, make_rms_signature, make_rms_signatures, make_households
+from .waveforms import make_periods, make_oscillations, make_rms_cycle, make_rms_signature, make_rms_signatures, make_households
 from ._composers import Composer, HComposer
