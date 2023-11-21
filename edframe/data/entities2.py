9e955f7f98f9ebf9ba4360070d018855947315ec
arx7ti <arx7ti@gmail.com>
2023-11-21 19:54:57 +0300

    resample, align, avg cycle

--- edframe/data/entities2.py
+++ edframe/data/entities2.py
@@ -16,6 +16,10 @@ class Gen:
     def fs(self):
         return self._fs
 
+    @property
+    def data(self):
+        return self._data
+
     @classmethod
     def new(cls, *args, **kwargs):
         return cls(*args, **kwargs)
@@ -45,29 +49,60 @@ class VI(H):
     def __init__(self, v, i, fs, **kwargs) -> None:
         data = np.stack((v, i))
         self._is_aligned = kwargs.get('is_aligned', False)
+        self._dims = kwargs.get('dims', None)
         super().__init__(data, fs)
 
-    def align(self):
-        fitps = FITPS()
+    def is_aligned(self):
+        return self._is_aligned
+
+    def align(self, **kwargs):
+        fitps = FITPS(**kwargs)
+
         try:
             v, i = fitps(self.v, self.i, fs=self.fs)
+            dims = v.shape
+            v, i = v.ravel(), i.ravel()
         except NotEnoughPeriods:
             v, i = self.v, self.i
+            dims = 1, len(v)
 
-        return self.new(v, i, self.fs, is_aligned=True)
+        return self.new(v, i, self.fs, is_aligned=True, dims=dims)
 
     def resample(self, fs, **kwargs):
         if fs > self.fs:
             v = upsample(self.v, self.fs, fs, **kwargs)
             i = upsample(self.i, self.fs, fs, **kwargs)
         elif fs < self.fs:
-            v = downsample(self.v, fs)
-            i = downsample(self.i, fs)
+            v = downsample(self.v, self.fs, fs)
+            i = downsample(self.i, self.fs, fs)
         else:
             v = self.v
             i = self.i
 
-        return self.new(v, i, fs, is_aligned=self._is_aligned)
+        return self.new(v,
+                        i,
+                        fs,
+                        is_aligned=self.is_aligned(),
+                        dims=self._dims)
+
+    def cycle(self, mode='mean', **kwargs):
+        if not self.is_aligned():
+            self = self.align(**kwargs)
+
+        dims = self._dims
+        data = self.data.reshape(2, *dims)
+
+        if mode == 'mean':
+            data = np.mean(data, axis=1)
+        elif mode == 'median':
+            data = np.median(data, axis=1)
+        else:
+            raise ValueError
+
+        v, i = data
+        dims = 1, len(v)
+
+        return self.new(v, i, self.fs, is_aligned=self.is_aligned(), dims=dims)
 
 
 class P(L):
