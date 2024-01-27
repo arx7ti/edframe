5e0fb990d86673171a7fc32a8290300046d0d648
arx7ti <arx7ti@gmail.com>
2024-01-27 03:12:00 +0300

    redo extrapolate

--- edframe/signals/_transforms.py
+++ edframe/signals/_transforms.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from scipy.signal import resample
-from scipy.interpolate import interp1d
 from typing import Union, Any, Callable
+from scipy.interpolate import interp1d
+from scipy.signal import resample, find_peaks
 from statsmodels.tsa.ar_model import AutoReg
 
 import math
@@ -74,10 +74,10 @@ def pad(
     axis: int = -1,
 ) -> np.ndarray:
     # TODO pad with values
-    if isinstance(n, int):
-        a, b = 0, n
-    else:
+    if isinstance(n, tuple):
         a, b = n
+    else:
+        a, b = n - n // 2, n // 2
 
     if axis < 0:
         axis = len(x.shape) + axis
@@ -152,155 +152,141 @@ def extrapolate2d(x, n, **kwargs):
 
     n_orig, n_samples = x.shape
 
-    # Generate cycles for extrapolation interval
-    x_extra = make_hf_cycles_from(x, n_samples=n)
-    x_extra = x_extra / abs(x_extra).max(1, keepdims=True)
+    # Replicate if only one cycle presents
+    if len(x) == 1:
+        x0 = 0
+        x_extra = np.repeat(x, n, axis=0)
+    else:
+        # Generate cycles for extrapolation interval
+        x_extra = make_hf_cycles_from(x, n_samples=n)
+        x_extra = x_extra / abs(x_extra).max(1, keepdims=True)
 
-    # Estimate amplitude envelope on the extrapolation interval
-    x0 = x.mean(-1, keepdims=True)
-    x = x - x0
-    a = abs(x).max(1)
-    t = np.linspace(0, 1, n_orig)
-    kind = kwargs.get('kind', 'linear')
-    n_orig_flat, n_extra_flat = np.product(x.shape), np.product(x_extra.shape)
-    t_flat = np.linspace(0, 1, n_orig_flat)
-    a_flat = interp1d(t, a, kind=kind)(t_flat)
-    a_extra = AutoReg(a_flat, 1).fit().predict(n_orig_flat,
-                                               n_orig_flat + n_extra_flat - 1)
-    a_extra = a_extra.reshape(-1, n_samples)
+        # Estimate amplitude envelope on the extrapolation interval
+        x0 = x.mean(-1, keepdims=True)
+        x = x - x0
+        a = abs(x).max(1)
+        t = np.linspace(0, 1, n_orig)
+        kind = kwargs.get('kind', 'linear')
+        n_orig_flat, n_extra_flat = np.product(x.shape), np.product(
+            x_extra.shape)
+        t_flat = np.linspace(0, 1, n_orig_flat)
+        a_flat = interp1d(t, a, kind=kind)(t_flat)
+        a_extra = AutoReg(a_flat,
+                          1).fit().predict(n_orig_flat,
+                                           n_orig_flat + n_extra_flat - 1)
+        a_extra = a_extra.reshape(-1, n_samples)
 
-    # Apply envelope
-    x_extra = a_extra * x_extra
+        # Apply envelope
+        x_extra = a_extra * x_extra
 
     # Combine original and extrapolated signals
+    # FIXME x0 for x_extra?
     x = np.concatenate((x + x0, x_extra))
 
     return x
 
 
-def extrapolate(x, n, v=None, **kwargs):
-    # FIXME if only one signal and non-aligned
+# @staticmethod
+# @njit
+def sync_cycles(
+    x: np.ndarray,
+    x0: np.ndarray,
+    dx: np.ndarray,
+    ns_int: int,
+) -> np.ndarray:
+    ns_float = x.dtype.type(ns_int)
+    n_periods = len(dx) - 1
+    mat = np.zeros((n_periods, ns_int), dtype=x.dtype)
+    x0 = x0.astype(x.dtype)
+
+    for j in np.arange(n_periods, dtype=np.int32):
+        length = (x0[j + 1] + dx[j + 1]) - (x0[j] + dx[j])
+        dist = length / ns_float
+
+        for k in np.arange(ns_int, dtype=np.int32):
+            k1 = x0[j] + dx[j] + dist * x.dtype.type(k)
+            k2 = np.int32(np.floor(k1))
+            k3 = np.int32(np.ceil(k1))
+            mat[j, k] = x[k2] + (x[k3] - x[k2]) * dx[j]
+    return mat
+
+
+def compute_shifts(v: np.ndarray, v0: np.ndarray) -> np.ndarray:
+    dv = np.empty(0, dtype=v.dtype)
+
+    for j in np.arange(len(v0) - 1, dtype=np.int32):
+        k = v0[j]
+        dv = np.append(dv, -v[k] / (v[k + 1] - v[k]))
+
+    return dv
+
+
+def extrapolate(x, n, x0=None, fs=None, f0=None, **kwargs):
+    '''
+    Main assumption: constant frequency
+    '''
+    if isinstance(n, tuple):
+        a, b = n
+    else:
+        a, b = n - n // 2, n // 2
+
     is_aligned = len(x.shape) == 2
 
-    # if not is_aligned and v is None:
-    #     z = np.fft.rfft(x, axis=)
-    #     v =
-
-    if not is_aligned:
+    if is_aligned:
+        n_orig = np.product(x.shape)
+        xs = x
+        da, db = 0, 0
+    else:
         n_orig = len(x)
 
+        if x0 is None:
+            assert fs is not None
+            assert f0 is not None
+            T = int(round(fs / f0))
+            # TODO if not whole cycles (amp issue)
+            x0 = np.arange(0, len(x), T)
+            dx = np.zeros(len(x0) - 1)
+        else:
+            dx = compute_shifts(x, x0)
+            T = round(np.diff(x0).mean())
+
         # Obtain synchronized signal with equal number of samples per cycle
-        fitps = FITPS()
-        v2d, x2d = fitps(v, x)
-        v0 = fitps.zero_crossings
+        xs = sync_cycles(x, x0, dx, T)
 
-        # Compensate missing cycles from the right after FITPS
-        dright = n_orig - v0[-2]
-    else:
-        n_orig = np.product(x.shape)
-        v2d, x2d = v, x
-        v0 = None
-        dright = 0
+        # Compensate missing cycles
+        da = x0[0]
+        db = n_orig - x0[-2]
 
-    # Number of cycles that are basis for extrapolation
-    n_obs, n_samples = x2d.shape
     # Number of cycles to extrapolate
-    n_extra = math.ceil((n + dright) / n_samples)
-    # Number of samples in the final signal
-    n_full = n + n_orig
+    n_samples = xs.shape[1]
+    na = math.ceil((a + da) / n_samples)
+    nb = math.ceil((b + db) / n_samples)
 
-    v2d = v2d if v2d is None else extrapolate2d(v2d, n_extra, **kwargs)
-    x2d = extrapolate2d(x2d, n_extra, **kwargs)
+    # Generate extra cycles
+    xe = make_hf_cycles_from(xs, n_samples=na + nb)
+    xe /= abs(xe).max(1, keepdims=True)
+    xa, xb = xe[:na], xe[na:]
 
-    if not is_aligned:
-        # Keep only extrapolated interval
-        v2d = v2d[-n_extra:]
-        x2d = x2d[-n_extra:]
+    amp = abs(xs).max(1)
+    n_cycles = len(xs)
+    areg1 = AutoReg(amp[::-1], 1).fit()
+    areg2 = AutoReg(amp, 1).fit()
 
-        # Extrapolate frequency fluctuations
-        dv0 = np.diff(v0)
-        dv0 = AutoReg(dv0, 1).fit().predict(n_obs, n_obs + n_extra - 1)
-        dv0 = np.round(dv0).astype(int)
+    if na > 0:
+        ampa = areg1.predict(n_cycles, n_cycles + na - 1)[::-1]
+        xa = (xa * ampa[:, None])
 
-        # Apply frequency fluctuations over extrapolated signal
-        t_aligned = np.linspace(0, 1, n_samples)
-        crop = True
+    if nb > 0:
+        ampb = areg2.predict(n_cycles, n_cycles + nb - 1)
+        xb = (xb * ampb[:, None])
 
-        for vi, xi, n_samples_i in zip(v2d, x2d, dv0):
-            if n_samples_i != n_samples:
-                ti = np.linspace(0, 1, n_samples_i)
-                vi = interp1d(t_aligned, vi)(ti)
-                xi = interp1d(t_aligned, xi)(ti)
+    x = np.concatenate((xa, xs, xb)).ravel()
 
-            if dright > 0 and crop:
-                vi = vi[dright:]
-                xi = xi[dright:]
-                crop = False
-
-            v = np.concatenate((v, vi))
-            x = np.concatenate((x, xi))
-    else:
-        v = v if v is None else v2d.ravel()
-        x = x2d.ravel()
-
-    del v2d, x2d
-
-    if n_full < len(x):
-        v = v if v is None else v[:n_full]
-        x = x[:n_full]
-
-    if v is not None:
-        return v, x
+    # Restrict the number of samples in the final signal
+    x = x[(n_samples - a) % n_samples:]
+    x = x[:n_orig + a + b]
 
     return x
-
-
-# class F:
-
-#     def __init__(self, fn: Callable, map_out_args, **map_in_args) -> None:
-
-#         if len(map_out_args) == 0:
-#             raise ValueError
-
-#         params = inspect.signature(fn).parameters.keys()
-
-#         for param in map_in_args.keys():
-#             if param not in params:
-#                 raise ValueError
-
-#         self._fn = fn
-#         self._map_out_args = map_out_args
-#         self._map_in_args = map_in_args
-
-#     def __call__(self, ps: PowerSample) -> PowerSample:
-
-#         data = {}
-
-#         for fparam, attr in self._map_in_args.items():
-#             if hasattr(ps, attr):
-#                 data.update({fparam: getattr(ps, attr)})
-#             else:
-#                 raise ValueError("Parameter \"%s\" was not found" % attr)
-
-#         result = self._fn(**data)
-
-#         if not isinstance(result, tuple):
-#             result = (result, )
-
-#         if len(result) < len(self._map_out_args):
-#             raise ValueError
-
-#         data = {}
-
-#         for attr, v in zip(self._map_out_args, result):
-#             if hasattr(ps, attr):
-#                 data.update({attr: v})
-#             else:
-#                 raise ValueError
-
-#         ps = ps.update(**data)
-
-#         return ps
 
 
 def fryze(v, i):
