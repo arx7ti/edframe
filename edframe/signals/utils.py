48b156e7b9eb43fb29f4ae3f2f5dbbd8ff711e1b
arx7ti <arx7ti@gmail.com>
2024-02-22 17:53:24 +0300

    sync high sampling rate recordings

--- edframe/signals/utils.py
+++ edframe/signals/utils.py
@@ -4,9 +4,12 @@ import os
 import math
 import pickle
 import random
+import warnings
 import numpy as np
 import pandas as pd
 import itertools as it
+
+from tqdm import tqdm
 from copy import deepcopy
 from numbers import Number
 from inspect import isfunction
@@ -16,30 +19,46 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from statsmodels.tools.sm_exceptions import MissingDataError
 
 from ._fitps import FITPS
+from ..utils.exceptions import OutliersDetected
 
 
-def list_f0(recordings):
-    return list({f0 for _, _, _, f0, *_ in recordings})
+def nearest_f0(recordings):
+    f0_list = list({f0 for _, _, _, f0, *_ in recordings})
+    f0 = int(round(np.mean(f0_list)))
+
+    return f0
 
 
-def nearest_f0(f0_list):
-    f0_list = [round(f0) for f0 in f0_list]
-
-    if len(set(f0_list)) > 1:
-        raise ValueError
-
-    return f0_list[0]
-
-
-def sync_recordings(recordings, f0_ref):
-    fitps = FITPS()
+def sync_recordings(
+    recordings,
+    window_size=1,
+    outlier_thresh=0.1,
+    zero_thresh=1e-4,
+    f0_ref=None,
+):
+    fitps = FITPS(window_size=window_size,
+                  outlier_thresh=outlier_thresh,
+                  zero_thresh=zero_thresh)
     sync_recordings = []
 
-    for v, i, fs, f0, appliances, locs in recordings:
-        assert abs(f0 - f0_ref) < 1
+    if f0_ref is None:
+        f0_ref = nearest_f0(recordings)
 
-        v, i = fitps(v, i, locs=locs, fs=fs, f0=f0_ref)
-        v, i = v.ravel(), i.ravel()
-        sync_recordings.append((v, i, fs, f0, appliances, locs))
+    for v, i, fs, _, appliances, locs in tqdm(recordings):
+        try:
+            fitps.fit(v)
+        except OutliersDetected:
+            continue
+        else:
+            T = math.ceil(fs / f0_ref)
+            v = fitps.transform(v, cycle_size=T, locs=locs)
+            i = fitps.transform(i, cycle_size=T, locs=locs)
+            v, i = v.ravel(), i.ravel()
+            sync_recordings.append((v, i, fs, f0_ref, appliances, locs))
+
+    dn = len(recordings) - len(sync_recordings)
+
+    if dn > 0:
+        warnings.warn(f'{dn} outliers were omitted.')
 
     return sync_recordings
